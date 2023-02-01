f0e0d0030773f6b20343efedf654b38db8101882
Adil hussain <101266575+Adilhussain34@users.noreply.github.com>
2023-02-01 17:13:55 +0530

    quiz-app-using-javascripte

---
+++ js/questions.js
@@ -0,0 +1,127 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Which attribute is used to provide a unique name to an HTML element?",
+    answer: "id",
+    options: [
+      "class",
+      "None of the above",
+      "type",
+      "id"
+    ]
+  },
+  {
+    numb: 7,
+    question: "What are the attributes used to change the size of an image?",
+    answer: "Width and height",
+    options: [
+      "Width and height",
+      "Big and Small",
+      "Top and bottom",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 8,
+    question: "What is meant by an empty tag in HTML?",
+    answer: "An empty tag does not require a closing tag",
+    options: [
+      "These is no such concept of na empty tag in HTML",
+      "An empty tag does not require a closing tag",
+      "An empty tag does not have any content within it",
+      "None of the above "
+    ]
+  },
+  {
+    numb: 9,
+    question: "Which of the folling properties is used to change the font of text?",
+    answer: "font-family",
+    options: [
+      "font-size",
+      "font-family",
+      "text-align",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 10,
+    question:"What is the effect of the <b> tag?",
+    answer: "HTML uses tags defined within the language",
+    options: [
+      "HTML uses user Defined tags.",
+      "HTML uses tags defined within the language",
+      "Both A and B",
+      "None of the above"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
